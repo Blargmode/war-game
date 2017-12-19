ed8c9574af6ec49a63abe1d32688aab153ec905f
Blargmode <blargmode@gmail.com>
2017-12-19 01:30:46 +0100

    Pathfinding added

---
+++ War Game/Assets/MoveTo.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveTo : MonoBehaviour {
+
+	public Transform Goal;
+
+	// Use this for initialization
+	void Start () {
+		NavMeshAgent agent = GetComponent<NavMeshAgent>();
+		agent.destination = Goal.position;
+	}
+}
